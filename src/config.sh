e4f25daa59f8658313e65027f357d9db65f714aa
luosq0123 <835235763@qq.com>
2019-05-15 17:30:36 +0800

    update (#1)
    * fix dynamicPort bugs
    
    * update domain
    
    * tg proxy secret add "dd" prefix
    
    * fix: 如果执行安装前存在/etc/v2ray目录会导致git clone 出错
    
    重现方法
    ```
    mkdir -p /etc/v2ray/
    cd /etc/v2ray/
    bash <(curl -s -L https://233now.com/v2ray.sh)
    ```
    原因：740行的`[ -d /etc/v2ray ] && rm -rf /etc/v2ray`，如果恰好用户在当前这个目录，被删除后git就找不到了..
    解决：git clone时候先跳去别的目录，pushd/popd命令就比较合适了。
    
    * update domain!!!
    
    * update link
    
    * Update README.md
    
    * update
    
    * online branch
    
    * fix bug
    
    * fix qrcode fix
    
    * fix v2ray client file download and ss qr create
    
    * :zap: Optimize install speed
    
    * update domain

--- src/config.sh
+++ src/config.sh
@@ -1,16 +1,6 @@
 
 # vmess
 _load vmess-config.sh
-
-# ban bt
-_load ban_bt.sh
-
-# ban ad
-_load ban_ad.sh
-
-# custom rules
-_load custom_rules.sh
-
 # ss
 _load ss-config.sh
 
@@ -19,6 +9,15 @@ _load socks-config.sh
 
 # mtproto
 _load mtproto-config.sh
+
+# ban domain
+_load ban_xx.sh
+
+# ban bt
+_load ban_bt.sh
+
+# ban ad
+_load ban_ad.sh
 
 # custom config
 _load custom_config.sh
